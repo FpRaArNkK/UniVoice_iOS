c9e5bfa208ca4cbd28220efd146aeb8f443054e3
kingjeongkong <wjdqls9223@gmail.com>
2024-07-03 05:46:50 +0900

    [Setting/#7] 함수 주석 추가

--- UniVoice/UniVoice/Global/Extension/UIButton+.swift
+++ UniVoice/UniVoice/Global/Extension/UIButton+.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 extension UIButton {
+    /// 버튼의 타이틀과 폰트, 타이틀 색상을 설정하는 메서드
+    /// - Parameters:
+    ///   - title: 설정할 타이틀 텍스트
+    ///   - font: PretendardFont 열거형으로 폰트 타입 설정
+    ///   - titleColor: 타이틀 색상
     func setTitle(_ title: String, font: UIFont.PretendardFont, titleColor: UIColor) {
         setAttributedTitle(.pretendardAttributedString(for: font, with: title), for: .normal)
         setTitleColor(titleColor, for: .normal)
     }
     
+    /// 버튼의 테두리 너비, 테두리 색상, 코너 반경을 설정하는 메서드
+    /// - Parameters:
+    ///   - borderWidth: 테두리 너비 (기본값: 0)
+    ///   - borderColor: 테두리 색상
+    ///   - cornerRadius: 코너 반경
     func setLayer(borderWidth: CGFloat = 0, borderColor: UIColor, cornerRadius: CGFloat) {
         layer.borderWidth = borderWidth
         layer.borderColor = borderColor.cgColor
         layer.cornerRadius = cornerRadius
     }
     
+    /// 버튼의 타이틀에 밑줄을 추가하는 메서드
     func addUnderline() {
         let titleLabel = self.titleLabel?.attributedText ?? NSAttributedString(string: self.title(for: .normal) ?? "")
         let attributedString = NSMutableAttributedString(attributedString: titleLabel)
@@ -31,6 +42,12 @@ extension UIButton {
 
 // Custom Font
 extension UIButton {
+    /// 커스텀 타이틀 텍스트와 폰트 크기, 가중치, 행간을 설정하는 메서드
+    /// - Parameters:
+    ///   - text: 설정할 타이틀 텍스트
+    ///   - fontSize: 폰트 크기
+    ///   - weight: 폰트 가중치
+    ///   - lineHeight: 행간
     func setCustomTitle(_ text: String, size fontSize: CGFloat, weight: UIFont.Weight, lineHeight: CGFloat) {
         self.titleLabel?.font = UIFont.customFont(size: fontSize, weight: weight)
         
--- UniVoice/UniVoice/Global/Extension/UILabel+.swift
+++ UniVoice/UniVoice/Global/Extension/UILabel+.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 extension UILabel {
+    /// 텍스트와 폰트, 색상을 설정하는 메서드
+    /// - Parameters:
+    ///   - text: 설정할 텍스트
+    ///   - font: PretendardFont 열거형으로 폰트 타입 설정
+    ///   - color: 텍스트 색상 설정
     func setText(_ text: String, font: UIFont.PretendardFont, color: UIColor) {
         self.attributedText = .pretendardAttributedString(for: font, with: text)
         self.textColor = color
@@ -16,6 +21,12 @@ extension UILabel {
 
 // Custom Font
 extension UILabel {
+    /// 커스텀 텍스트와 폰트 크기, 가중치, 행간을 설정하는 메서드
+    /// - Parameters:
+    ///   - text: 설정할 텍스트
+    ///   - fontSize: 폰트 크기
+    ///   - weight: 폰트 가중치
+    ///   - lineHeight: 행간
     func setCustomText(_ text: String, size fontSize: CGFloat, weight: UIFont.Weight, lineHeight: CGFloat) {
         self.font = UIFont.customFont(size: fontSize, weight: weight)
         
