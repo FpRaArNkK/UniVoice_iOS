0a0c85eb35f25365195f49999fbe1ddeacdc93e7
FpRaArNkK <mspark1184@naver.com>
2024-07-06 00:12:13 +0900

    [Feat/#13] CustomButton init

---
+++ UniVoice/UniVoice/Presentation/Components/CustomButton.swift
@@ -0,0 +1,47 @@
+//
+//  CustomButton.swift
+//  UniVoice
+//
+//  Created by 박민서 on 7/6/24.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+    // MARK: Properties
+    // MARK: Views
+    
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpFoundation()
+        setUpHierarchy()
+        setUpUI()
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpFoundation
+    private func setUpFoundation() {
+        
+    }
+    
+    // MARK: setUpHierarchy
+    private func setUpHierarchy() {
+        
+    }
+    
+    // MARK: setUpUI
+    private func setUpUI() {
+        
+    }
+    
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+}
