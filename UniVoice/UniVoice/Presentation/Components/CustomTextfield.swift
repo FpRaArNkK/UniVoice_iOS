f254106aeb2198c0e9247afdf4815ef27840a44a
FpRaArNkK <mspark1184@naver.com>
2024-07-06 03:00:47 +0900

    [Feat/#13] CustomTextField 컴포넌트 구현

--- UniVoice/UniVoice/Presentation/Components/CustomTextfield.swift
+++ UniVoice/UniVoice/Presentation/Components/CustomTextfield.swift
@@ -13,16 +13,64 @@ import RxCocoa
 
 class CustomTextfield: UITextField {
     
+    // MARK: Properties
+    private var disposeBag: DisposeBag?
+    private let borderLayer = CALayer()
+    private let activeColor = UIColor.mint600
+    private let inactiveColor = UIColor.gray200
+    
+    // MARK: - Layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        borderLayer.frame = CGRect(x: 0, y: frame.size.height - 2, width: frame.size.width, height: 2)
+    }
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        bindUI()
+        setUpUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func caretRect(for position: UITextPosition) -> CGRect {
+        var originalRect = super.caretRect(for: position)
+        originalRect.size.width = 2.0
+        return originalRect
+    }
+    
+    // MARK: setUpUI
+    private func setUpUI() {
+        borderLayer.borderWidth = 2.0
+        borderLayer.borderColor = inactiveColor.cgColor
+        borderLayer.frame = CGRect(x: 0, y: frame.size.height - 2, width: frame.size.width, height: 2)
+        layer.addSublayer(borderLayer)
+        borderStyle = .none
+        tintColor = activeColor
+    }
+    
+    // MARK: bindUI
     private func bindUI() {
+        guard let disposeBag = self.disposeBag else { return }
+        
+        Observable.merge(
+            self.rx.controlEvent([.editingDidBegin]).map { true },
+            self.rx.controlEvent([.editingDidEnd]).map { false }
+        )
+        .bind(onNext: { [weak self] isEditing in
+            self?.borderLayer.borderColor = isEditing ? self?.activeColor.cgColor : self?.inactiveColor.cgColor
+            self?.setNeedsDisplay()
+        })
+        .disposed(by: disposeBag)
+    }
+}
+
+// MARK: External Logic
+extension CustomTextfield {
+    func bindData(with disposeBag: DisposeBag) {
+        self.disposeBag = disposeBag
+        bindUI()
     }
 }
