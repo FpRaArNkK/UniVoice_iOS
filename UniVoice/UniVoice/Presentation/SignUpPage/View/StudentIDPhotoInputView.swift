a7a48dc5267228381df60092fda58f79fde2ac68
kingjeongkong <wjdqls9223@gmail.com>
2024-07-07 15:35:06 +0900

    [Feat/#20] StudentIDPhotoInput 파일 생성

---
+++ UniVoice/UniVoice/Presentation/SignUpPage/View/StudentIDPhotoInputView.swift
@@ -0,0 +1,42 @@
+//
+//  StudentIDPhotoInputView.swift
+//  UniVoice
+//
+//  Created by 왕정빈 on 7/7/24.
+//
+
+import UIKit
+
+class StudentIDPhotoInputView: UIView {
+
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpFoundation()
+        setUpHierarchy()
+        setUpUI()
+        setUpLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: setUpFoundation
+    private func setUpFoundation() {
+
+    }
+    // MARK: setUpHierarchy
+    private func setUpHierarchy() {
+
+    }
+    // MARK: setUpUI
+    private func setUpUI() {
+
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+
+    }
+}
---
+++ UniVoice/UniVoice/Presentation/SignUpPage/ViewController/StudentIDPhotoInputVC.swift
@@ -0,0 +1,15 @@
+//
+//  StudentIDPhotoInputVC.swift
+//  UniVoice
+//
+//  Created by 왕정빈 on 7/7/24.
+//
+
+import UIKit
+
+class StudentIDPhotoInputVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
