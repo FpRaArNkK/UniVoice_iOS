5141190c1808112ad5d49430c99a8da27fcd49e3
FpRaArNkK <mspark1184@naver.com>
2024-07-07 23:42:19 +0900

    [Chore/#22] CustomTextField coder init 추가

--- UniVoice/UniVoice/Presentation/Components/CustomTextfield.swift
+++ UniVoice/UniVoice/Presentation/Components/CustomTextfield.swift
@@ -37,7 +37,9 @@ class CustomTextfield: UITextField {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        setUpUI()
+        bindUI()
     }
     
     override func caretRect(for position: UITextPosition) -> CGRect {
