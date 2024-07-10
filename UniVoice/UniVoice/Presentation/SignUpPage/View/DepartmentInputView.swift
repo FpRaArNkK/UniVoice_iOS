516b767a23dcb7767cf4b9b01937b211dbc798bf
jaminleee <jamin_00@naver.com>
2024-07-10 17:58:07 +0900

    [Feat/#21] DepartmentInputView UI

--- UniVoice/UniVoice/Presentation/SignUpPage/View/DepartmentInputView.swift
+++ UniVoice/UniVoice/Presentation/SignUpPage/View/DepartmentInputView.swift
@@ -10,6 +10,11 @@ import UIKit
 final class DepartmentInputView: UIView {
 
     // MARK: Views
+    let departInputLabel = UILabel()
+    let departTextField = CustomTextfield()
+    let departTableView = UITableView()
+    let nextButton = CustomButton()
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,18 +30,53 @@ final class DepartmentInputView: UIView {
 
     // MARK: setUpFoundation
     private func setUpFoundation() {
-
+        self.backgroundColor = .white
     }
     // MARK: setUpHierarchy
     private func setUpHierarchy() {
-
+        [
+            departInputLabel,
+            departTextField,
+            departTableView,
+            nextButton
+        ].forEach { self.addSubview($0) }
     }
     // MARK: setUpUI
     private func setUpUI() {
-
+        departInputLabel.do {
+            $0.setText("학과를 입력해주세요", font: .T1SB, color: .B_01)
+        }
+        
+        departTextField.do {
+            $0.placeholder = "학과 이름 검색하기"
+        }
+        
+        nextButton.do {
+            $0.setTitle("다음", for: .normal)
+        }
     }
     // MARK: setUpLayout
     private func setUpLayout() {
-
+        departInputLabel.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(36)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        departTextField.snp.makeConstraints {
+            $0.top.equalTo(departInputLabel.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(33)
+        }
+        
+        departTableView.snp.makeConstraints {
+            $0.top.equalTo(departTextField.snp.bottom)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.keyboardLayoutGuide.snp.top).offset(-12)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(53)
+        }
     }
 }
