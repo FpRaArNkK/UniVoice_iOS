6efd996bc936f93736f16ec987bedd4035b8ed09
FpRaArNkK <mspark1184@naver.com>
2024-07-11 02:56:52 +0900

    [Feat/#16] CapsuleLabelView 작성

---
+++ UniVoice/UniVoice/Presentation/Components/CapsuleLabelView.swift
@@ -0,0 +1,91 @@
+//
+//  CapsuleLabelView.swift
+//  UniVoice
+//
+//  Created by 박민서 on 7/11/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class CapsuleLabelView: UIView {
+    
+    // MARK: - Properties
+    private let contentLabel = UILabel()
+    
+    // MARK: - Init
+    private override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    init(
+        with name: String,
+        font: UIFont = .pretendardFont(for: .BUT4SB),
+        color: UIColor = .gray800
+    ) {
+        super.init(frame: .zero)
+        setupView()
+        setTitle(title: name, font: font)
+        setColor(color: color)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    // MARK: setUpView
+    private func setupView() {
+        self.backgroundColor = .white
+        
+        contentLabel.text = ""
+        contentLabel.font = .pretendardFont(for: .BUT4SB)
+        contentLabel.textAlignment = .center
+        
+        self.addSubview(contentLabel)
+        
+        contentLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 6, left: 10, bottom: 6, right: 10))
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+            super.draw(rect)
+            updateCornerRadius()
+        }
+        
+    private func updateCornerRadius() {
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.borderWidth = 1
+    }
+}
+
+// MARK: External Logic
+extension CapsuleLabelView {
+    func setTitle(title: String, font: UIFont) {
+        self.contentLabel.text = title
+        self.contentLabel.font = font
+    }
+    
+    func setColor(color: UIColor) {
+        self.contentLabel.textColor = color
+        self.layer.borderColor = color.cgColor
+    }
+    
+    func setInset(inset: UIEdgeInsets) {
+        contentLabel.snp.remakeConstraints {
+            $0.edges.equalToSuperview().inset(inset)
+        }
+    }
+}
+
+//@available(iOS 17.0, *)
+//#Preview {
+//    PreviewController(CapsuleLabelView(with: "실험"), snp: { view in
+//        view.snp.makeConstraints {
+//            $0.center.equalToSuperview()
+//        }
+//    })
+//}
