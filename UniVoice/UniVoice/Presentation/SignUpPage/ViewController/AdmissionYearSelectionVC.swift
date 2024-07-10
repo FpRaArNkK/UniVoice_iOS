c0a6535820ac534ba5ae5da7825eea63ac8b4a60
jaminleee <jamin_00@naver.com>
2024-07-10 18:24:27 +0900

    [Feat/#21] AdmissionYearSelectionVC

--- UniVoice/UniVoice/Presentation/SignUpPage/ViewController/AdmissionYearSelectionVC.swift
+++ UniVoice/UniVoice/Presentation/SignUpPage/ViewController/AdmissionYearSelectionVC.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class AdmissionYearSelectionVC: UIViewController {
 
     // MARK: Views
     private let rootView = AdmissionYearSelectionView()
+    private let disposeBag = DisposeBag() // 임시
 
     // MARK: Life Cycle - loadView
     override func loadView() {
@@ -21,10 +24,20 @@ final class AdmissionYearSelectionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpFoundation()
+        setUpBindUI()
     }
 
     // MARK: setUpFoundation
     private func setUpFoundation() {
-        self.title = "회원가입"
+        self.title = "개인정보입력U"
+    }
+    
+    // MARK: setUpBindUI
+    private func setUpBindUI() {
+        rootView.nextButton.rx.tap
+            .bind(onNext: { [weak self] in
+                self?.navigationController?.pushViewController(UnivInfoConfirmVC(), animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 }
