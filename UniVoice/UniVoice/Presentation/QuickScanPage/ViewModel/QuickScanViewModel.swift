1e85f9c89c9c23b6698638ee2eb0474a336d7726
FpRaArNkK <mspark1184@naver.com>
2024-07-11 17:55:38 +0900

    [Feat/#16] QuickScanViewModel init

---
+++ UniVoice/UniVoice/Presentation/QuickScanPage/ViewModel/QuickScanViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  QuickScanViewModel.swift
+//  UniVoice
+//
+//  Created by 박민서 on 7/11/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class QuickScanViewModel: ViewModelType {
+    
+    struct Input {
+    }
+    
+    struct Output {
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
+
+// MARK: API Logic
+extension QuickScanViewModel {
+}
