6ca67991c2d28ab2dcb86821a37522c2c5cb2e84
jaminleee <jamin_00@naver.com>
2024-07-13 04:27:21 +0900

    [Feat/#37] CustomButton 높이, 간격 수정

--- UniVoice/UniVoice/Presentation/SignUpPage/View/AdmissionYearSelectionView.swift
+++ UniVoice/UniVoice/Presentation/SignUpPage/View/AdmissionYearSelectionView.swift
@@ -153,9 +153,9 @@ final class AdmissionYearSelectionView: UIView {
         }
         
         nextButton.snp.makeConstraints {
-            $0.bottom.equalTo(self.safeAreaLayoutGuide)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-16)
             $0.horizontalEdges.equalToSuperview().inset(16)
-            $0.height.equalTo(53)
+            $0.height.equalTo(57)
         }
     }
     
