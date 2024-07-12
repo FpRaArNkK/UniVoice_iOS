5ce163c9e275e4c148054e62c5abfa4b1c9c3333
FpRaArNkK <mspark1184@naver.com>
2024-07-12 22:31:59 +0900

    [Feat/#16] QuickScanViewController UI 로직 작성

--- UniVoice/UniVoice/Presentation/QuickScanPage/ViewController/QuickScanViewController.swift
+++ UniVoice/UniVoice/Presentation/QuickScanPage/ViewController/QuickScanViewController.swift
@@ -16,7 +16,7 @@ final class QuickScanViewController: UIViewController {
     
     // MARK: Properties
     private let rootView = QuickScanView()
-    private let disposeBag = DisposeBag()
+    private let viewModel = QuickScanViewModel()
     
     // MARK: Life Cycle - loadView
     override func loadView() {
@@ -38,58 +38,91 @@ final class QuickScanViewController: UIViewController {
     
     // MARK: setUpBindUI
     private func setUpBindUI() {
-        let quickScanItems = Observable.just([
-            "아주대학교 총학생회", "소프트웨어융합대학 학생회", "디지털미디어학과 학생회", "아주대학교 총학생회"
-        ])
         
-        let quickScanDataSource = RxCollectionViewSectionedReloadDataSource<SectionModel<String, String>>(
+        let changeIndex = rootView.quickScanContentCollectionView.rx.contentOffset
+            .map { $0.x }
+            .distinctUntilChanged()
+            .map({ [weak self] offsetX -> Int in
+                guard let self = self else { return 0 }
+                
+                let pageWidth = self.rootView.quickScanContentCollectionView.frame.width
+                guard pageWidth > 0 else { return 0 }
+                
+                let currentPage = Int((offsetX + pageWidth / 2) / pageWidth)
+                return currentPage
+            })
+            .distinctUntilChanged()
+        
+        rootView.quickScanContentCollectionView.rx.contentOffset
+            .map { $0.x }
+            .filter { [weak self] offsetX in
+                guard let self = self else { return false }
+                let collectionView = self.rootView.quickScanContentCollectionView
+                let collectionViewWidth = collectionView.contentSize.width - collectionView.frame.width
+                return offsetX > collectionViewWidth
+            }
+            .subscribe(onNext: { [weak self] _ in
+                self?.pushNextVC()
+            })
+            .disposed(by: viewModel.disposeBag)
+        
+        let bookmarkDidTap = PublishRelay<Int>()
+        
+        let input = QuickScanViewModel.Input(
+            changeIndex: changeIndex,
+            bookmarkDidTap: bookmarkDidTap.asObservable()
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        let quickScanDataSource = RxCollectionViewSectionedReloadDataSource<SectionModel<String, QuickScan>>(
             configureCell: { _, collectionView, indexPath, item in
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: QuickScanContentCVC.identifier, for: indexPath) 
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: QuickScanContentCVC.identifier, for: indexPath)
                         as? QuickScanContentCVC else { return UICollectionViewCell() }
-                cell.fetchData(cellModel: .init(
-                    affiliationImageURL: "https://example.com/image.png",
-                    affiliationName: "소프트웨어융합대학",
-                    createdTime: Date(),
-                    viewCount: 123,
-                    noticeTitle: "2024학년도 신입생 환영회",
-                    noticeTarget: "전체 학생",
-                    startTime: Calendar.current.date(byAdding: .day, value: 1, to: Date()),
-                    endTime: Calendar.current.date(byAdding: .day, value: 2, to: Date()),
-                    content: "2024학년도 신입생 환영회가 개최됩니다. 모든 신입생은 참여 바랍니다.",
-                    isScrapped: true
-                ))
+                cell.fetchData(cellModel: item)
+                cell.bindTapEvent(relay: bookmarkDidTap, index: indexPath.row)
+                
+                let isScrapped = output.quickScans.asObservable().map { $0[indexPath.row].isScrapped }
+                cell.bindUI(isScrapped: isScrapped)
                 return cell
             }
         )
         
-        quickScanItems
+        output.quickScans
             .map { [SectionModel(model: "QuickScanSection", items: $0)] }
-            .bind(to: rootView.quickScanContentCollectionView.rx.items(dataSource: quickScanDataSource))
-            .disposed(by: disposeBag)
+            .drive(rootView.quickScanContentCollectionView.rx.items(dataSource: quickScanDataSource))
+            .disposed(by: viewModel.disposeBag)
         
-        rootView.quickScanContentCollectionView.rx.contentOffset
-            .map { $0.x }
-            .distinctUntilChanged()
-            .bind(onNext: { [weak self] offsetX in
-                guard let self = self else { return }
-                
-                let pageWidth = self.rootView.quickScanContentCollectionView.frame.width
-                guard pageWidth > 0 else { return }
-                
-                let currentPage = Int((offsetX + pageWidth / 2) / pageWidth)
-                print("Current Page: \(currentPage)")
-            })
-            .disposed(by: disposeBag)
+        let quickScanCount = output.quickScans
+            .map { $0.count }
+            .asObservable()
+        
+        let currentIndex = output.currentIndex.asObservable()
+        
+        rootView.indicatorView.bindData(
+            quickScanCount: quickScanCount,
+            currentIndex: currentIndex
+        )
+        
     }
 }
 
+// MARK: UICollectionViewDelegateFlowLayout
 extension QuickScanViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(
         _ collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAt indexPath: IndexPath
     ) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height) // 원하는 셀 크기 지정
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+}
+
+// MARK: Internal Logic
+private extension QuickScanViewController {
+    func pushNextVC() {
+        let nextVC = ViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
 
