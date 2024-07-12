851e5d5f728a938e59f5eadbd43c40762fc3a1af
kingjeongkong <wjdqls9223@gmail.com>
2024-07-13 05:06:54 +0900

    [Feat/#34] TOS 비즈니스 로직 구현

--- UniVoice/UniVoice/Presentation/SignUpPage/ViewController/TOSCheckVC.swift
+++ UniVoice/UniVoice/Presentation/SignUpPage/ViewController/TOSCheckVC.swift
@@ -6,24 +6,82 @@
 //
 
 import UIKit
+import RxSwift
 
 class TOSCheckVC: UIViewController {
     
     // MARK: - Properties
     private let rootView = TOSCheckView()
-
+    private let viewModel = TOSCheckVM()
+    
     // MARK: - Life Cycle
     override func loadView() {
         self.view = rootView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpFoundation()
+        setUpBindUI()
     }
     
     // MARK: - setUpFoundation
     private func setUpFoundation() {
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func setUpBindUI() {
+        let input = TOSCheckVM.Input(
+            overallAgreeCheckBoxDidTap: rootView.overallAgreeCheckBox.rx.tap.asObservable(),
+            serviceTermsCheckBoxDidTap: rootView.serviceTermsCheckBox.rx.tap.asObservable(),
+            personalInfoTOSCheckBoxDidTap: rootView.personalInfoTOSCheckBox.rx.tap.asObservable()
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        let completeButtonState = output.completeButtonState
+            .map { $0 ? CustomButtonType.active : CustomButtonType.inActive }
+        
+        output.overallAgreeCheckBoxState
+            .drive { [weak self] checkBoxState in
+                switch checkBoxState {
+                case .checked:
+                    self?.rootView.overallAgreeCheckBox.setImage(.icnCheckedBox, for: .normal)
+                case .unchecked:
+                    self?.rootView.overallAgreeCheckBox.setImage(.icnUncheckedBox, for: .normal)
+                }
+            }
+            .disposed(by: viewModel.disposeBag)
+        
+        output.serviceTermsCheckBoxState
+            .drive { [weak self] checkBoxState in
+                switch checkBoxState {
+                case .checked:
+                    self?.rootView.serviceTermsCheckBox.setImage(.icnCheckedBox, for: .normal)
+                case .unchecked:
+                    self?.rootView.serviceTermsCheckBox.setImage(.icnUncheckedBox, for: .normal)
+                }
+            }
+            .disposed(by: viewModel.disposeBag)
+        
+        output.personalInfoTOSCheckBoxState
+            .drive { [weak self] checkBoxState in
+                switch checkBoxState {
+                case .checked:
+                    self?.rootView.personalInfoTOSCheckBox.setImage(.icnCheckedBox, for: .normal)
+                case .unchecked:
+                    self?.rootView.personalInfoTOSCheckBox.setImage(.icnUncheckedBox, for: .normal)
+                }
+            }
+            .disposed(by: viewModel.disposeBag)
+        
+        rootView.completeButton.bindData(buttonType: completeButtonState.asObservable())
+        
+        rootView.completeButton.rx.tap
+            .observe(on: MainScheduler.instance)
+            .bind { [weak self] _ in
+                self?.navigationController?.pushViewController(SignUpInfoCheckingVC(), animated: true)
+            }
+            .disposed(by: viewModel.disposeBag)
     }
 }
