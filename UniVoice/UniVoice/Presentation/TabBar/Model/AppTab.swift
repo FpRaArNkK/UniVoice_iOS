d9cf3d58582870780eea600915d9730c82226fc4
luckyyy <oyslucy@naver.com>
2024-07-14 02:39:05 +0900

    [Feat/#26] TabBarVC

--- UniVoice/UniVoice/Presentation/TabBar/Model/AppTab.swift
+++ UniVoice/UniVoice/Presentation/TabBar/Model/AppTab.swift
@@ -5,4 +5,32 @@
 //  Created by 오연서 on 7/14/24.
 //
 
-import Foundation
+import UIKit
+
+enum AppTab: String, CaseIterable {
+    case home = "홈"
+    case save = "저장"
+    case mypage = "설정"
+    
+    var image: UIImage {
+        switch self {
+        case .home:
+            return .homeIc
+        case .save:
+            return .saveIc
+        case .mypage:
+            return .mypageIc
+        }
+    }
+    
+    var selectedImage: UIImage {
+        switch self {
+        case .home:
+            return .homeFilledIc.withRenderingMode(.alwaysOriginal)
+        case .save:
+            return .saveFilledIc.withRenderingMode(.alwaysOriginal)
+        case .mypage:
+            return .mypageFilledIc.withRenderingMode(.alwaysOriginal)
+        }
+    }
+}
