9c8de0c57f9410160f23802cc0bcd99d902f67b3
kingjeongkong <wjdqls9223@gmail.com>
2024-07-16 01:33:22 +0900

    [Feat/#72] TargetType 파일 생성

---
+++ UniVoice/UniVoice/Data/Router/TargetType/UserTargetType.swift
@@ -0,0 +1,35 @@
+//
+//  UserTargetType.swift
+//  UniVoice
+//
+//  Created by 왕정빈 on 7/16/24.
+//
+
+import Foundation
+import Moya
+
+enum UserTargetType {
+    
+}
+
+extension UserTargetType: ServiceTargetType {
+    var baseURL: URL {
+        return URL(string: "test")!
+    }
+    
+    var path: String {
+        return "test"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
