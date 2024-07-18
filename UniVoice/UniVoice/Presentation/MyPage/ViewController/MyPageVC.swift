31e75cfd186dfd46c60574f5e3b053aa56f7bdd7
FpRaArNkK <mspark1184@naver.com>
2024-07-18 17:04:38 +0900

    [Feat/#109] 마이페이지 API 연결 완료

--- UniVoice/UniVoice/Presentation/MyPage/ViewController/MyPageVC.swift
+++ UniVoice/UniVoice/Presentation/MyPage/ViewController/MyPageVC.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import Kingfisher
 import RxSwift
 import RxCocoa
 import RxDataSources
 
 final class MyPageVC: UIViewController, UIScrollViewDelegate {
     
+    // MARK: Properties
+    private let myInfo = BehaviorRelay<MyPage>(value: .init(id: 0, name: "", collegeDepartment: "", department: "", admissionNumber: "", university: "", universityLogoImage: ""))
+    private let disposeBag = DisposeBag()
+    
     // MARK: Views
     private let rootView = MyPageView()
-    
+        
     // MARK: Life Cycle - loadView
     override func loadView() {
         self.view = rootView
@@ -24,5 +29,23 @@ final class MyPageVC: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
+        
+        self.getMyPageInfo()
+            .bind(onNext: { data in
+                self.rootView.nameLabel.text = data.name
+                self.rootView.universityLabel.text = data.university
+                let imageURL = URL(string: data.universityLogoImage)
+                self.rootView.universityImage.kf.setImage(with: imageURL)
+                self.rootView.collegeDepartmentLabel.text = data.collegeDepartment
+                self.rootView.departmentLabel.text = "\(data.department) \(data.admissionNumber)"
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension MyPageVC {
+    func getMyPageInfo() -> Observable<MyPage> {
+        return Service.shared.getMyPage().asObservable()
+            .map { $0.data }
     }
 }
