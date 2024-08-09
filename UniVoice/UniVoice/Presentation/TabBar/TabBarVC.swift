b491ae3cdb2ddd4586f1a02dd0af998a47fa8ffd
FpRaArNkK <mspark1184@naver.com>
2024-08-09 15:57:24 +0900

    [Chore/#165] TabBar AppTab Model -> TabBarVC 상단으로 이동

--- UniVoice/UniVoice/Presentation/TabBar/TabBarVC.swift
+++ UniVoice/UniVoice/Presentation/TabBar/TabBarVC.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+enum AppTab: String, CaseIterable {
+    case home = "홈"
+    case save = "저장"
+    case mypage = "설정"
+    
+    var image: UIImage {
+        switch self {
+        case .home:
+            return .homeIc
+        case .save:
+            return .saveIc
+        case .mypage:
+            return .mypageIc
+        }
+    }
+    
+    var selectedImage: UIImage {
+        switch self {
+        case .home:
+            return .homeFilledIc.withRenderingMode(.alwaysOriginal)
+        case .save:
+            return .saveFilledIc.withRenderingMode(.alwaysOriginal)
+        case .mypage:
+            return .mypageFilledIc.withRenderingMode(.alwaysOriginal)
+        }
+    }
+}
+
 final class TabBarVC: UITabBarController, UITabBarControllerDelegate {
     
     // MARK: Life Cycle - viewDidLoad
