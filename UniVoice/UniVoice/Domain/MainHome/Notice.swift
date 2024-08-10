5e533342accc7a721f40352b55af4acdbd4cfdda
luckyyy <oyslucy@naver.com>
2024-08-10 21:24:16 +0900

    [Chore/#171] property 주석 추가

--- UniVoice/UniVoice/Domain/MainHome/Notice.swift
+++ UniVoice/UniVoice/Domain/MainHome/Notice.swift
@@ -9,11 +9,18 @@ import UIKit
 
 /// MainHome 페이지에서 사용하는  Notice 모델입니다.
 struct Notice {
+    /// 공지 id
     let id: Int
+    /// 공지 종류(선착순 or 공지사항)
     let chip: String
+    /// 공지 제목
     let noticeTitle: String
+    /// 공지 이미지
     let thumbnailImage: String
-    let duration: String
+    /// 작성 시간
+    let createdTime: String
+    /// 좋아요 수
     let likedNumber: Int
+    /// 저장 수
     let savedNumber: Int
 }
--- UniVoice/UniVoice/Domain/MainHome/QuickScanProfile.swift
+++ UniVoice/UniVoice/Domain/MainHome/QuickScanProfile.swift
@@ -9,7 +9,10 @@ import UIKit
 
 /// MainHome 페이지에서 사용하는  QuickScanProfile 모델입니다.
 struct QuickScanProfile {
+    /// 학생회 이미지
     let councilImage: String
+    /// 학생회 이름
     let councilName: String
-    let noticeNumber: Int // 읽지 않은 공지 개수
+    /// 읽지 않은 공지 개수
+    let noticeNumber: Int
 }
