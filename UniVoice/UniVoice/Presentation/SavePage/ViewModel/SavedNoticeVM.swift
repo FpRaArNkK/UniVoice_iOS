83ca8ba39607a781ff2dced895438d72bd709c15
FpRaArNkK <mspark1184@naver.com>
2024-08-17 12:06:56 +0900

    [Docs/#183] SavedNoticePage 주석 추가

--- UniVoice/UniVoice/Presentation/SavePage/ViewModel/SavedNoticeVM.swift
+++ UniVoice/UniVoice/Presentation/SavePage/ViewModel/SavedNoticeVM.swift
@@ -12,23 +12,28 @@ import UIKit
 final class SavedNoticeVM: ViewModelType {
     
     struct Input {
+        /// 새로고침 이벤트
         let refreshEvent: Observable<Void>
     }
     
     struct Output {
+        /// 공지 목록 데이터
         let listData: Driver<[Notice]>
+        /// 새로고침 종료 트리거
         let refreshQuitTrigger: Driver<Void>
     }
     
     var disposeBag = DisposeBag()
     
     func transform(input: Input) -> Output {
+        // 새로고침 이벤트에 따라 저장된 공지 목록을 가져옴
         let listData = input.refreshEvent
             .flatMapLatest({ [weak self] in
                 self?.getSavedList() ?? .just([])
             })
             .asDriver(onErrorJustReturn: [])
         
+        // 공지 목록 데이터를 받아 새로고침 종료 트리거를 활성화
         let refreshQuit = listData.map { _ in Void() }
             .asDriver()
         
@@ -38,7 +43,6 @@ final class SavedNoticeVM: ViewModelType {
 
 // MARK: API Logic
 extension SavedNoticeVM {
-
     private func getSavedList() -> Observable<[Notice]> {
         return Service.shared.getSavedNoticeList().asObservable()
             .map { $0.data.map { $0.toNotice() } }
